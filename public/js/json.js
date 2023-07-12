964bd5afe6f38193521b596c1c244059ecec547e
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-07-12 17:44:18 -0400

    correciones

--- public/js/json.js
+++ public/js/json.js
@@ -4,8 +4,12 @@ const apiExamen=async(pagina)=>{
     const api= await fetch(url);
     const data=await api.json();
     console.log(data);
+    divRes=document.querySelector("#dato");
+    divRes.innerHTML=`${data.total}`
+    
+
     divRes=document.querySelector("#resultado");
-    divRes.innerHTML=""
+    divRes.innerHTML=``
     // el foreach es para el bucle :)
     data.usuarios.forEach(usuarios=> {
         // esto es para que haga el bucle del array :
@@ -17,8 +21,6 @@ const apiExamen=async(pagina)=>{
             <td>${usuarios.correo}</td>
             <td>${usuarios.rol}</td>
             <td>${usuarios.estado}</td>
-            
-
             `
             divRes.appendChild(divItem);
     });
